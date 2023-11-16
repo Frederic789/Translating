8c051a3d5c58b47560497ee0bb7f1abc25c6a22f
Freddy <fredericdouamba2014@gmail.com>
2023-11-15 17:49:12 -0800

    Initial Commit

---
+++ Localization/LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationApp.swift
+//  Localization
+//
+//  Created by Student Account on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
